b902d27f834ae35e1d24eaed99b0711cafd543b4
Belka-S <belka.srg@gmail.com>
2023-06-26 18:05:33 +0200

    add mongo db

--- server.js
+++ server.js
@@ -6,7 +6,6 @@ const { DB_HOST } = process.env;
 const PORT = 3001;
 
 // Run server
-
 mongoose
   .connect(DB_HOST)
   .then(app.listen(PORT, () => console.log(`Server is running on port ${PORT}`)))
